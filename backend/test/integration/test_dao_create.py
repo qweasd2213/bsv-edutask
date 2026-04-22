e86c193d59dcc22f2b91a9c0e50caf2707c998be
qweasd2213 <qweasd2213@proton.me>
2026-04-23 00:56:32 +0200

    todo and video

--- backend/test/integration/test_dao_create.py
+++ backend/test/integration/test_dao_create.py
@@ -27,8 +27,7 @@ def test_db():
     client = pymongo.MongoClient(test_mongo_url)
     client.drop_database(db_name)
 
-    # ta bort test URLen så det inte trollar
-    del os.environ['MONGO_URL']
+
 
     #
     #
@@ -134,6 +133,183 @@ def test_create_duplicate_email(test_db):
     with pytest.raises(Exception):
         dao.create(user_data_2)
 
+
+
+@pytest.mark.integration
+def test_create_valid_task_with_optionals(test_db):
+    dao = DAO("task")
+
+    obj1 = ObjectId()
+    obj2 = ObjectId()
+    obj3 = ObjectId()
+    obj4 = ObjectId()
+
+
+    task_data = {
+        "title": "watch video 1",
+        "description": "video 1 contains school material",
+        "startdate": datetime(2026, 4, 22),
+        "duedate": datetime(2026, 4, 26),
+        "requires": [obj1],
+        "categories": ["video", "school"],
+        "todos": [obj2, obj3],
+        "video": obj4
+    }
+
+    result = dao.create(task_data)
+
+    assert "_id" in result
+    assert result["title"] == "watch video 1"
+    assert result["description"] == "video 1 contains school material"
+    assert result["startdate"]["$date"] == "2026-04-22T00:00:00Z"
+    assert result["duedate"]["$date"] == "2026-04-26T00:00:00Z"
+    assert result["requires"][0]["$oid"] == str(obj1)
+    assert result["categories"] == ["video", "school"]
+    assert result["todos"][0]["$oid"] == str(obj2)
+    assert result["todos"][1]["$oid"] == str(obj3)
+    assert result["video"]["$oid"] == str(obj4)
+
+@pytest.mark.integration
+def test_create_valid_task_without_optionals(test_db):
+    dao = DAO("task")
+
+    task_data = {
+        "title": "watch video 2",
+        "description": "video 2 contains school material"
+    }
+
+    result = dao.create(task_data)
+
+    assert "_id" in result
+    assert result["title"] == "watch video 2"
+    assert result["description"] == "video 2 contains school material"
+
+@pytest.mark.integration
+def test_create_task_duplicate_title():
+    dao = DAO("task")
+
+    task_data_1 = {
+        "title": "watch video 3",
+        "description": "video 3 contains fun"
+    }
+
+    task_data_2 = {
+        "title": "watch video 3",
+        "description": "video 3 contains fun"
+    }
+
+    dao.create(task_data_1)
+
+    with pytest.raises(Exception):
+        dao.create(task_data_2)
+
+#TODO
+@pytest.mark.integration
+def test_create_todo():
+    dao = DAO("todo")
+
+    todo_data = {
+        "description": "Walk the dog",
+        "done": True
+    }
+
+    result = dao.create(todo_data)
+
+    assert "_id" in result
+    assert result["description"] == "Walk the dog"
+    assert result["done"] == True
+
+@pytest.mark.integration
+def test_create_todo_duplicate_description():
+    dao = DAO("todo")
+
+    todo_data = {
+        "description": "Walk the dog",
+        "done": True
+    }
+
+    todo_data_duplicate = {
+        "description": "Walk the dog",
+        "done": True
+    }
+
+    dao.create(todo_data)
+
+    with pytest.raises(Exception):
+        dao.create(todo_data_duplicate)
+
+@pytest.mark.integration
+def test_create_todo_wrong_type():
+    dao = DAO("todo")
+
+    todo_data = {
+        "description": 123,
+        "done": True
+    }
+
+    with pytest.raises(Exception):
+        dao.create(todo_data)
+
+@pytest.mark.integration
+def test_create_todo_missing_required_field():
+    dao = DAO("todo")
+
+    todo_data = {
+        "done": True
+    }
+
+    with pytest.raises(Exception):
+        dao.create(todo_data)
+
+#VIDEO
+@pytest.mark.integration
+def test_create_video():
+    dao = DAO("video")
+
+    video_data = {
+        "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    }
+
+    result = dao.create(video_data)
+
+    assert "_id" in result
+    assert result["url"] == "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+@pytest.mark.integration
+def test_create_video_wrong_type():
+    dao = DAO("video")
+
+    video_data = {
+        "url": False
+    }
+
+    with pytest.raises(Exception):
+        dao.create(video_data)
+
+@pytest.mark.integration
+def test_create_video_missing_required_field():
+    dao = DAO("video")
+
+    video_data = {
+    }
+
+    with pytest.raises(Exception):
+        dao.create(video_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # According to documentation these edgecases might be to much:
 
 # @pytest.mark.integration
@@ -187,71 +363,3 @@ def test_create_duplicate_email(test_db):
     # TASK TESTS
     #
     #
-
-@pytest.mark.integration
-def test_create_valid_task_with_optionals(test_db):
-    dao = DAO("task")
-
-    obj1 = ObjectId()
-    obj2 = ObjectId()
-    obj3 = ObjectId()
-    obj4 = ObjectId()
-
-
-    task_data = {
-        "title": "watch video 1",
-        "description": "video 1 contains school material",
-        "startdate": datetime(2026, 4, 22),
-        "duedate": datetime(2026, 4, 26),
-        "requires": [obj1],
-        "categories": ["video", "school"],
-        "todos": [obj2, obj3],
-        "video": obj4
-    }
-
-    result = dao.create(task_data)
-
-    assert "_id" in result
-    assert result["title"] == "watch video 1"
-    assert result["description"] == "video 1 contains school material"
-    assert result["startdate"]["$date"] == "2026-04-22T00:00:00Z"
-    assert result["duedate"]["$date"] == "2026-04-26T00:00:00Z"
-    assert result["requires"][0]["$oid"] == str(obj1)
-    assert result["categories"] == ["video", "school"]
-    assert result["todos"][0]["$oid"] == str(obj2)
-    assert result["todos"][1]["$oid"] == str(obj3)
-    assert result["video"]["$oid"] == str(obj4)
-
-@pytest.mark.integration
-def test_create_valid_task_without_optionals(test_db):
-    dao = DAO("task")
-
-    task_data = {
-        "title": "watch video 2",
-        "description": "video 2 contains school material"
-    }
-
-    result = dao.create(task_data)
-
-    assert "_id" in result
-    assert result["title"] == "watch video 2"
-    assert result["description"] == "video 2 contains school material"
-
-@pytest.mark.integration
-def test_create_task_duplicate_title(test_db):
-    dao = DAO("task")
-
-    task_data_1 = {
-        "title": "watch video 3",
-        "description": "video 3 contains fun"
-    }
-
-    task_data_2 = {
-        "title": "watch video 3",
-        "description": "video 3 contains fun"
-    }
-
-    dao.create(task_data_1)
-
-    with pytest.raises(Exception):
-        dao.create(task_data_2)
